c9f22d007be125b019b94aa1d06cd7a5bd87ca11
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-06-08 10:01:21 +0100

    Create Starting Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import StartingPage from "./components/StartingPage";
 
 function App() {
-
+    return (
+        <main>
+            <StartingPage />
+        </main>
+    )
 }
 
 export default App;
---
+++ src/components/StartingPage.jsx
@@ -0,0 +1,9 @@
+export default function StartingPage() {
+    return (
+        <section className="starting-page">
+            <h1>Quizzical</h1>
+            <p>The ultimate random quiz</p>
+            <button>Start quiz</button>
+        </section>
+    );
+}
